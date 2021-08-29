549ece2c98e7d11756843d27c828ba19978fd7d8
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-29 18:04:33 -0500

    stripped app from styling to port in new design from figma

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Chat from './components/Chat'
 import Login from './components/Login'
 import Header from './components/Header'
-import Sidebar from './components/Sidebar'
+// import Sidebar from './components/Sidebar'
+import Sidebar from './model_ui/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className="appContainer">
-          <Header />
-          <div className="appMain">
+        <Switch>
+          <Route path="/">
             <Sidebar />
-            <Switch>
-              <Route path="/room">
-                <Chat />
-              </Route>
-              <Route path="/">
-                <Login />
-              </Route>
-            </Switch>
-          </div>
-        </div>
+          </Route>
+          <Route path="/room">
+            {/* <Sidebar /> */}
+            <Chat />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
