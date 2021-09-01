51c959e9695e7cbd4961d682cadc97c6b4f4f4ac
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-01 15:49:41 -0500

    add build directory for reacty

--- server/server.js
+++ server/server.js
@@ -38,15 +38,15 @@ io.on('connection', (socket) => {
   });
 });
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// } else {
-//   app.use(express.static(path.join(__dirname, '../client/public')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+} else {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/public/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join('index.html'));
+});
 
 db.once('open', () => {
   console.log(PORT, typeof (PORT), PORT + 10, parseInt(PORT) + 10)
