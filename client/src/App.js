b21342c076bfd3390806d991578bf845348ff1e5
mguppy <mguppy34@gmail.com>
2021-09-02 09:56:58 -0500

    updated app.js to correct graphql link to be able to login

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
