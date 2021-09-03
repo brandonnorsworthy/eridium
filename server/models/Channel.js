bee54e959141d95d96fd07d7c8a259a37abe15c0
mguppy <mguppy34@gmail.com>
2021-09-03 13:47:25 -0500

    channel DB things added

---
+++ server/models/Channel.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const channelSchema = new Schema({
+    channel_name: {
+      type: String,
+      required: 'Please enter a channel name!',
+      minlength: 1,
+      maxlength: 100,
+      trim: true,
+    },
+    server: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Server"
+      }
+    ],
+  });
+  
+  const Server = model('Channel', channelSchema);
+  
+  module.exports = Channel;
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Message = require('./Message');
 const Server = require('./Server');
+const Channel = require('./Channel');
 
-module.exports = { User, Message, Server };
+module.exports = { User, Message, Server, Channel };
