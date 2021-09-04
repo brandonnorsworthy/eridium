3e2d90f4aeedf9306e7ecf08f6efafa9577678fa
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-09-04 01:45:38 -0500

    commenting out tags to find error

--- client/src/App.js
+++ client/src/App.js
@@ -57,7 +57,7 @@ function App() {
   return (
     /* go to localhost:3000/demo for an example of what it will look like */
     /* components have the templates that will be used for javascript do not touch /demo folder */
-    <ApolloProvider client={client}>
+    // <ApolloProvider client={client}>
       <div className="App">
         <Router>
           <Switch>
@@ -73,7 +73,7 @@ function App() {
             </Route>
             <Route path="/">
               {/* DO NOT DELETE */}
-              {checkAuth()}
+              {/* {checkAuth()} */}
               {/* <Route>
                 <Sidebar />
                 <Content />
@@ -82,7 +82,7 @@ function App() {
           </Switch>
         </Router>
       </div>
-    </ApolloProvider>
+    {/* </ApolloProvider> */}
   );
 }
 
